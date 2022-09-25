defaf1346beb9bc97437ee78e8973cf013194406
Srimathi Elango <86002860+SrimathiElango21@users.noreply.github.com>
2022-09-25 13:06:51 +0530

    phase 4 End Project

---
+++ src/app/crud/crud.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud',
+  templateUrl: './crud.component.html',
+  styleUrls: ['./crud.component.css']
+})
+export class CrudComponent implements OnInit {
+   newEmployeeClicked=false;
+   employees=[
+    {name:'sri',position:'developer'},
+    {name:'Mathi',position:'Designer'}
+   ];
+   color:any;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  model:any={};
+  model2:any={};
+  addEmployee(){
+    this.employees.push(this.model);
+    this.model={};
+  }
+
+  deleteEmployee(i:any){
+    this.employees.splice(i);
+    console.log(i);
+  }
+myValue:any;
+
+editEmployee(editEmployeeInfo:any){
+  this.model2.name=this.employees[editEmployeeInfo].name;
+  this.model2.position=this.employees[editEmployeeInfo].position;
+  this.myValue=editEmployeeInfo;
+}
+
+updateEmployee(){
+  let editEmployeeInfo=this.myValue;
+  for(let i=0;i<this.employees.length;i++){
+    if(i==editEmployeeInfo){
+      this.employees[i]=this.model2;
+      this.model2={}
+    }
+  }
+}
+
+addNewEmployeeBtn(){
+  this.newEmployeeClicked= !this.newEmployeeClicked;
+}
+
+changeColorOne(){
+  this.color= !this.color;
+  if(this.color){
+    return '#ffffff';
+  }else{
+    return '#f6f6f6';
+  }
+}
+}
